3628ea39fc48d87cac3cecb87f5127b4becb12be
igorivochkin <igor_ivochkin@mail.ru>
2024-12-02 08:13:20 +0300

    Add files via upload

---
+++ module_9_1.py
@@ -0,0 +1,11 @@
+#module_9_1.py
+
+def apply_all_func(int_list, *functions):
+    result = {}
+    for i in functions:
+      result[i.__name__] = i(int_list)
+    return result
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
